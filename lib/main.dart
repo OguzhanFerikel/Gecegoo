757410dcba1531d68adc8899d0f1a587df3f900e
OguzhanFerikel <122794945+OguzhanFerikel@users.noreply.github.com>
2023-02-14 14:02:59 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red.shade300,
         appBar: AppBar(
+            centerTitle: true,
             title: const Text("Gecego Bebektir"),
             backgroundColor: Colors.grey.shade500),
         body: Center(
